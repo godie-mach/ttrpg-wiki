97bc8c155cdce8c992fd12e20d772dbf6c8f72f7
godie-mach <dmchamorro@DMs-MacBook-Pro.local>
2024-10-18 13:03:30 -0500

    Quartz sync: Oct 18, 2024, 1:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "DnD Crew",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
