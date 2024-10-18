3d7ef7270ebba6e4b041be9c23b6eef4b3294bbd
godie-mach <dmchamorro@DMs-MacBook-Pro.local>
2024-10-18 14:35:08 -0500

    Quartz sync: Oct 18, 2024, 2:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Pirata One",
-        body: "Source Sans Pro",
+        body: "Poly",
         code: "IBM Plex Mono",
       },
       colors: {
